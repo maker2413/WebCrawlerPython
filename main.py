814f533c817760ada7c3a60a1423b655d4b7fca1
maker2413 <maker2413@gmail.com>
2026-04-25 16:56:48 -0500

    adding concurrency

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
-from crawl import crawl_page
+import asyncio
+from crawl import crawl_site_async
 
 
-def main():
+async def main():
     args = sys.argv
     if len(args) < 2:
         print("no website provided")
@@ -13,16 +14,15 @@ def main():
 
     base_url = args[1]
 
-    print(f"starting crawl of: {base_url}...")
+    print(f"Starting async crawl of: {base_url}")
 
-    page_data = crawl_page(base_url)
+    page_data = await crawl_site_async(base_url)
 
-    print(f"Found {len(page_data)} pages:")
     for page in page_data.values():
-        print(f"- {page['url']}: {len(page['outgoing_links'])} outgoing links")
+        print(f"Found {len(page['outgoing_links'])} outgoing links on {page['url']}")
 
     sys.exit(0)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
